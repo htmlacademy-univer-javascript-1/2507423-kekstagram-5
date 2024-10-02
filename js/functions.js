7e4ae3c85fa9ca6b63c2b4ad2ea12a9a29db8cd4
Сережа <13serdem13@gmail.com>
2024-10-03 02:55:26 +0500

    Добавляет функцию isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -2,4 +2,13 @@ const isStringLengthFit = function(string, maxLength) {
   return string.length <= maxLength;
 };
 
+const isPalindrome = function (string) {
+  let normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return reversedString === normalizedString;
+};
+
 
