c798c90a9181fd37bd4aa1fb425294559bda6e00
Сережа <13serdem13@gmail.com>
2024-10-21 22:07:55 +0500

    Создаст функцию генерации случайного целого положительного числа

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-const { create } = require('browser-sync');
+const { create, get } = require('browser-sync');
 
+
+function getRandomInteger (min, max) = {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return result;
+};
 
 const generatePhotoDescription = function () {
   return {
-    id: NaN,
+    id: generatePhotoId(),
     url: '',
     description: '',
     likes: NaN,
