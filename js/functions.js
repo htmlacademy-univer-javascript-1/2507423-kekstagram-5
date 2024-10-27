3bd3d61dd0ca09f554532ad90bd8ac90818e0668
Сережа <13serdem13@gmail.com>
2024-10-27 22:29:07 +0500

    Функция, определяющая, выходит ли встреча за рамки рабочего дня

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 const isStringLengthFit = function(string, maxLength) {
   return string.length <= maxLength;
 };
 
 const isPalindrome = function (string) {
-  let normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
   let reversedString = '';
   for (let i = normalizedString.length - 1; i >= 0; i--) {
     reversedString += normalizedString[i];
@@ -11,4 +12,18 @@ const isPalindrome = function (string) {
   return reversedString === normalizedString;
 };
 
+const isOverWorkingHours = function (workingDayStartTime, workingDayEndTime, meetingStartTime, meetingDuration) {
+  const timeToMinutes = (time) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+  const meetingStartInMinutes = timeToMinutes(meetingStartTime);
+  const workingDayEndInMinutes = timeToMinutes(workingDayEndTime);
+  const workingDayStartInMinutes = timeToMinutes(workingDayStartTime);
 
+  if (meetingStartInMinutes >= workingDayStartInMinutes && meetingStartInMinutes <= workingDayEndInMinutes) {
+    return (workingDayEndInMinutes >= meetingStartInMinutes + meetingDuration);
+  } else {
+    return false;
+  }
+};
