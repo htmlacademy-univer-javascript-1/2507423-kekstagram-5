a4276b091b9e92d230f26ecc0f559c6280610a5f
weirdlame <13serdem13@gmail.com>
2025-04-10 17:55:21 +0500

    Добавлено заполнение данными каждого элемента массива фотографий

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -6,7 +6,10 @@ const picturesContainer = document.querySelector('.pictures');
 
 const thumbnailsGallery = createPhotoGallery();
 
-thumbnailsGallery.forEach(() => {
+thumbnailsGallery.forEach((thumbnail) => {
   const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = thumbnail.url;
+  pictureElement.querySelector('.picture__likes').textContent = thumbnail.likes;
+  pictureElement.querySelector('.picture__comments').textContent = thumbnail.comments.length;
   picturesContainer.append(pictureElement);
 });
