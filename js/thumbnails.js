c65b3eeadae9489510d6615ad2a2e6af483d0b52
weirdlame <13serdem13@gmail.com>
2025-04-10 18:21:43 +0500

    Добавлена вставка элементов в контейнер через фрагмент

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -2,15 +2,20 @@ import { createPhotoGallery } from './data.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const picturesContainer = document.querySelector('.pictures');
+const picturesContainerElement = document.querySelector('.pictures');
 
 const thumbnailsGallery = createPhotoGallery();
 
+const picturesContainerFragment = document.createDocumentFragment();
 
 thumbnailsGallery.forEach((thumbnail) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = thumbnail.url;
   pictureElement.querySelector('.picture__likes').textContent = thumbnail.likes;
   pictureElement.querySelector('.picture__comments').textContent = thumbnail.comments.length;
-  picturesContainer.append(pictureElement);
+  picturesContainerElement.append(picturesContainerFragment);
 });
+
+picturesContainerElement.append(picturesContainerFragment);
+
+
