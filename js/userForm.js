0747fa1ce7ff18c3f68bfd3d84262064b2bc6b2a
weirdlame <13serdem13@gmail.com>
2025-04-19 15:44:14 +0500

    Улучшена структура кода, повторяющаяся логика вынесена в отдельные функции

--- js/userForm.js
+++ js/userForm.js
@@ -1,9 +1,33 @@
 const imageUploadForm = document.querySelector('#upload-select-image');
 const imageUploadFile = document.querySelector('#upload-file');
 const imageUploadOverlay = document.querySelector('.img-upload__overlay');
+const imageUploadCancel = document.querySelector('#upload-cancel');
 
-imageUploadFile.addEventListener('change', () => {
+function openUploadForm() {
   imageUploadForm.classList.remove('hidden');
   imageUploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+}
+
+function closeUploadForm() {
+  imageUploadOverlay.classList.add('hidden');
+  imageUploadFile.value = '';
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentEscapeKeydown);
+}
+
+function onDocumentEscapeKeydown(evt) {
+  if (evt.key === 'Escape') {
+    closeUploadForm();
+  }
+}
+
+imageUploadFile.addEventListener('change', () => {
+  openUploadForm();
+
+  document.addEventListener('keydown', onDocumentEscapeKeydown);
 });
 
+imageUploadCancel.addEventListener('click', () => {
+  closeUploadForm();
+});
